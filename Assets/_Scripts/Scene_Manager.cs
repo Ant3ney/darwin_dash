5806d8c8def6de1596e7942d7fbcd77fe47ba7a7
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-15 14:03:45 -0700

    Add more to scene_manager

--- Assets/_Scripts/Scene_Manager.cs
+++ Assets/_Scripts/Scene_Manager.cs
@@ -8,6 +8,11 @@ public class Scene_Manager : MonoBehaviour
     private static Scene_Manager _instance;
     public static Scene_Manager Instance { get { return _instance; } }
 
+    // format for mathtype: "mathType_numDigits"
+    public static string mathType = "addition_1";
+    public float highScore = 0;
+    const string HIGH_SCORE_KEY = "highScore";
+
     private void Awake()
     {
         manageSingleton();
