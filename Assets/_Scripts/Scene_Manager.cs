0deb7c78f525d2aef136e62764687fc5482d3e4c
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-04-15 13:33:30 -0700

    Add math scene

---
+++ Assets/_Scripts/Scene_Manager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene_Manager : MonoBehaviour
+{
+   /// <summary>
+   /// This is a basic template for a C# script in Unity, with empty Start and Update functions.
+   /// </summary>
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
